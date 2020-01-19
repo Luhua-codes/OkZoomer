b0f672ca987f3151bb436a428ece64717c34d166
JJ-Chang <jada.books@gmail.com>
2020-01-19 01:27:03 -0500

    First commit

---
+++ buttons.js
@@ -0,0 +1,8 @@
+/*God and anime*/
+var audio = "Audio/animemes/GodAnime.mp3";
+var GodAnime = new Audio();
+GodAnime.src = "Audio/animemes/GodAnime.mp3";
+
+function PlaySound() {
+  GodAnime.play(); /*will be changed to array index*/
+}
